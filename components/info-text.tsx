81a61f4a14599019c1660f9679f8158850152b9b
ton1czech <tonyasek007@gmail.com>
2024-01-13 21:56:44 +0100

    🎁NEW: info text component

---
+++ components/info-text.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { useLanguage } from '@/store/use-language'
+import Link from 'next/link'
+import { FC } from 'react'
+
+interface Props {
+  textCz: string
+  textEn: string
+  textDe: string
+  textUa: string
+  href?: string
+  linkCz?: string
+  linkEn?: string
+  linkDe?: string
+  linkUa?: string
+}
+
+export const InfoText: FC<Props> = ({
+  textCz,
+  textEn,
+  textDe,
+  textUa,
+  href,
+  linkCz,
+  linkEn,
+  linkDe,
+  linkUa,
+}) => {
+  const { language } = useLanguage()
+
+  return (
+    <span className='font-stabil text-xl lg:text-3xl'>
+      <>
+        {language === 'cz' && textCz}
+        {language === 'en' && textEn}
+        {language === 'de' && textDe}
+        {language === 'ua' && textUa}
+      </>{' '}
+      {href && (
+        <Link href={href} target='_blank' className='font-labil font-black'>
+          {language === 'cz' && linkCz}
+          {language === 'en' && linkEn}
+          {language === 'de' && linkDe}
+          {language === 'ua' && linkUa}
+        </Link>
+      )}
+    </span>
+  )
+}
--- containers/languages.tsx
+++ containers/languages.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Container } from '@/components/container'
+import { InfoText } from '@/components/info-text'
 import { languages } from '@/database/languages'
 import { useLanguage } from '@/store/use-language'
 import Link from 'next/link'
@@ -11,60 +12,39 @@ export const Languages = () => {
   return (
     <section id='languages' className='scroll-mt-44'>
       <Container>
-        <span className='font-stabil text-xl lg:text-3xl'>
-          {language === 'cz' && (
-            <>
-              Užij si svoje hodiny individuálně, ve dvojici nebo v malé skupince
+        <InfoText
+          textCz='Užij si svoje hodiny individuálně, ve dvojici nebo v malé skupince
               s max. 4 dalšími studenty. Nemusíš se nám upisovat na celé
               pololetí, rezervuj si klidně jenom 10 lekcí. Na osobní schůzce,
               online callu nebo telefonátu před zahájením kurzu spolu doladíme
               detaily. Všechny naše skupinové kurzy s volným místem pro tebe
-              máme přehledně vypsané{' '}
-            </>
-          )}
-          {language === 'en' && (
-            <>
-              Enjoy your classes either individually, in pairs or in small
+              máme přehledně vypsané'
+          textEn="Enjoy your classes either individually, in pairs or in small
               groups of no more than 5 students. You don't have to sign up for
               the whole semester immediately, but to start off you may book just
               10 lessons. We work out the details at a face-to-face meeting or
               during an online call or a phone call before the course begins. We
-              have all our available group courses for you listed{' '}
-            </>
-          )}
-          {language === 'de' && (
-            <>
-              Gestalte dir deine Stunden entweder individuell, zu zweit oder
+              have all our available group courses for you listed"
+          textDe='Gestalte dir deine Stunden entweder individuell, zu zweit oder
               auch in der Kleingruppe mit nicht mehr als vier Lernenden. Du
               musst dich bei uns nicht gleich für ein ganzes Semester
               einschreiben, buche ruhig erst einmal nur zehn Lektionen. Die
               Details können wir persönlich vor Kursbeginn besprechen, gerne
               auch per Videoanruf oder telefonisch. Alle unsere Gruppenkurse, in
               denen noch ein freier Platz für dich ist, haben wir hier
-              übersichtlich{' '}
-            </>
-          )}
-          {language === 'ua' && (
-            <>
-              Насолоджуйся своїми уроками індивідуально, в парах або в невеликій
+              übersichtlich'
+          textUa='Насолоджуйся своїми уроками індивідуально, в парах або в невеликій
               групі, котра складається з максимально з 4 учасників. Тобі не
               потрібно записуватись до нас на весь семестр, просто забронюй 10
               уроків. Ми разом узгодимо деталі під час особистої зустрічі,
               онлайн-дзвінка або телефонного дзвінка перед початком курсу. Усі
-              наші групові курси з вільним місцем чітко перераховані{' '}
-            </>
-          )}
-          <Link
-            href='https://linktr.ee/rozmluv.se'
-            target='_blank'
-            className='font-labil font-black'
-          >
-            {language === 'cz' && 'tady ←'}
-            {language === 'en' && 'here ←'}
-            {language === 'de' && 'aufgelistet ←'}
-            {language === 'ua' && 'тут ←'}
-          </Link>
-        </span>
+              наші групові курси з вільним місцем чітко перераховані'
+          href='https://linktr.ee/rozmluv.se'
+          linkCz='tady ←'
+          linkEn='here ←'
+          linkDe='aufgelistet ←'
+          linkUa='тут ←'
+        />
 
         <div className='grid lg:grid-cols-3 gap-8 mt-14'>
           {languages.map(item => (
