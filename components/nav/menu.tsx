a1072fbc655832a715bf68fdb9a1c2b4c71b1c8c
ton1czech <tonyasek007@gmail.com>
2024-01-13 17:16:32 +0100

    🎁NEW: menu with navlinks and sociallinks

--- components/nav/menu.tsx
+++ components/nav/menu.tsx
@@ -1,15 +1,52 @@
+'use client'
+
+import { links } from '@/database/links'
+import { useLanguage } from '@/store/use-language'
 import { useMenu } from '@/store/use-menu'
 import { X } from 'lucide-react'
+import Link from 'next/link'
 
 export const Menu = () => {
+  const { language } = useLanguage()
   const { closeMenu } = useMenu()
 
   return (
-    <div className='fixed right-0 top-0 h-full w-full lg:w-1/3 bg-primary'>
+    <div className='fixed right-0 top-0 h-full w-full lg:w-1/3 bg-primary grid place-content-center'>
       <X
         className='absolute top-6 right-6 cursor-pointer'
         onClick={() => closeMenu()}
       />
+      <ul className='flex flex-col gap-8'>
+        {links.map(link => (
+          <Link
+            href={link.href}
+            onClick={() => closeMenu()}
+            className='text-3xl font-bold text-center capitalize'
+          >
+            {language === 'cz' && link.labelCz}
+            {language === 'en' && link.labelEn}
+            {language === 'de' && link.labelDe}
+            {language === 'ua' && link.labelUa}
+          </Link>
+        ))}
+      </ul>
+      <div className='flex justify-between absolute bottom-6 left-0 w-full px-36'>
+        <Link href='https://www.facebook.com/rozmluv.se' target='_blank'>
+          <img src='/social/facebook.webp' />
+        </Link>
+        <Link href='https://www.instagram.com/rozmluv.se/' target='_blank'>
+          <img src='/social/instagram.webp' />
+        </Link>
+        <Link href='https://www.tiktok.com/@rozmluv.se' target='_blank'>
+          <img src='/social/tiktok.webp' />
+        </Link>
+        <Link
+          href='https://www.linkedin.com/company/rozmluv-se/?originalSubdomain=cz'
+          target='_blank'
+        >
+          <img src='/social/linkedin.webp' />
+        </Link>
+      </div>
     </div>
   )
 }
---
+++ database/links.ts
@@ -0,0 +1,44 @@
+export const links = [
+  {
+    labelCz: 'vyber jazyk',
+    labelEn: 'select language',
+    labelDe: 'Sprache wählen',
+    labelUa: 'вибір мови',
+    href: '#language',
+  },
+  {
+    labelCz: 'zapoj firmu',
+    labelEn: 'involve your company',
+    labelDe: 'Firma einbeziehen',
+    labelUa: 'приєднати фірму',
+    href: '#company',
+  },
+  {
+    labelCz: 'o nás',
+    labelEn: 'about us',
+    labelDe: 'Über uns',
+    labelUa: 'про нас',
+    href: '#about',
+  },
+  {
+    labelCz: 'reference',
+    labelEn: 'references',
+    labelDe: 'Referenzen',
+    labelUa: 'довідка',
+    href: '#references',
+  },
+  {
+    labelCz: 'ceník',
+    labelEn: 'pricelist',
+    labelDe: 'Preisliste',
+    labelUa: 'прайслист',
+    href: '#pricelist',
+  },
+  {
+    labelCz: 'kontakt',
+    labelEn: 'contact',
+    labelDe: 'Kontakt',
+    labelUa: 'контакт',
+    href: '#contact',
+  },
+]
