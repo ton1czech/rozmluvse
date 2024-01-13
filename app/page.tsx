59491e1de3976e618d6e57a70460e9011f524a38
ton1czech <tonyasek007@gmail.com>
2024-01-13 14:52:11 +0100

    🎁NEW: change title based on language

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
+'use client'
+
 import { Hero } from '@/containers/hero'
+import { useLanguage } from '@/store/use-language'
+import { useEffect } from 'react'
 
 export default function Page() {
+  const { language } = useLanguage()
+
+  useEffect(() => {
+    document.title =
+      language === 'cz'
+        ? 'Jazykové studio | rozmluv se'
+        : language === 'en'
+        ? 'Language studio | rozmluv se'
+        : language === 'de'
+        ? 'Sprachstudio | rozmluv se'
+        : language === 'ua'
+        ? 'мовна студія | rozmluv se'
+        : ''
+  }, [language])
+
   return (
     <>
       <main className='mt-28 md:mt-32 lg:mt-40'>
