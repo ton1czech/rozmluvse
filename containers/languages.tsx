e6982e16f5f25a76b697ce6665ad8066e8a656ab
ton1czech <danielanthonybaudys@gmail.com>
2024-03-02 06:11:35 +0500

    👌IMPROVE: languages using cols layout

--- containers/languages.tsx
+++ containers/languages.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { Cols } from '@/components/cols'
 import { Container } from '@/components/container'
 import { InfoText } from '@/components/info-text'
+import { SectionTitle } from '@/components/section-title'
 import { languages } from '@/database/languages'
 import { cn } from '@/lib/utils'
 import { useLanguage } from '@/store/use-language'
@@ -13,20 +15,27 @@ export const Languages = () => {
   return (
     <section id='languages' className='scroll-mt-28'>
       <Container>
-        <InfoText
-          textCz='Užij si svoje hodiny individuálně, ve dvojici nebo v malé skupince
+        <Cols>
+          <SectionTitle
+            titleCz='Vyber jazyk'
+            titleEn='Select language'
+            titleDe='Sprache wählen'
+            titleUa='вибір мови'
+          />
+          <InfoText
+            textCz='Užij si svoje hodiny individuálně, ve dvojici nebo v malé skupince
               s max. 4 dalšími studenty. Nemusíš se nám upisovat na celé
               pololetí, rezervuj si klidně jenom 10 lekcí. Na osobní schůzce,
               online callu nebo telefonátu před zahájením kurzu spolu doladíme
               detaily. Všechny naše skupinové kurzy s volným místem pro tebe
               máme přehledně vypsané'
-          textEn="Enjoy your classes either individually, in pairs or in small
+            textEn="Enjoy your classes either individually, in pairs or in small
               groups of no more than 5 students. You don't have to sign up for
               the whole semester immediately, but to start off you may book just
               10 lessons. We work out the details at a face-to-face meeting or
               during an online call or a phone call before the course begins. We
               have all our available group courses for you listed"
-          textDe='Gestalte dir deine Stunden entweder individuell, zu zweit oder
+            textDe='Gestalte dir deine Stunden entweder individuell, zu zweit oder
               auch in der Kleingruppe mit nicht mehr als vier Lernenden. Du
               musst dich bei uns nicht gleich für ein ganzes Semester
               einschreiben, buche ruhig erst einmal nur zehn Lektionen. Die
@@ -34,19 +43,20 @@ export const Languages = () => {
               auch per Videoanruf oder telefonisch. Alle unsere Gruppenkurse, in
               denen noch ein freier Platz für dich ist, haben wir hier
               übersichtlich'
-          textUa='Насолоджуйся своїми уроками індивідуально, в парах або в невеликій
+            textUa='Насолоджуйся своїми уроками індивідуально, в парах або в невеликій
               групі, котра складається з максимально з 4 учасників. Тобі не
               потрібно записуватись до нас на весь семестр, просто забронюй 10
               уроків. Ми разом узгодимо деталі під час особистої зустрічі,
               онлайн-дзвінка або телефонного дзвінка перед початком курсу. Усі
               наші групові курси з вільним місцем чітко перераховані'
-          href='https://linktr.ee/rozmluv.se'
-          external
-          linkCz='tady ←'
-          linkEn='here ←'
-          linkDe='aufgelistet ←'
-          linkUa='тут ←'
-        />
+            href='https://linktr.ee/rozmluv.se'
+            external
+            linkCz='tady ←'
+            linkEn='here ←'
+            linkDe='aufgelistet ←'
+            linkUa='тут ←'
+          />
+        </Cols>
 
         <div className='grid lg:grid-cols-3 gap-8 mt-14'>
           {languages.map(item => (
