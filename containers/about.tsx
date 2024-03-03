4607c080536e3c0c5e6a49c1daa928866b54bd63
ton1czech <danielanthonybaudys@gmail.com>
2024-03-03 18:20:06 +0500

    👌IMPROVE: about 2 cols redesign

--- containers/about.tsx
+++ containers/about.tsx
@@ -2,14 +2,12 @@
 
 import { Container } from '@/components/container'
 import { InfoText } from '@/components/info-text'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination } from 'swiper/modules'
-import 'swiper/css'
-import 'swiper/css/pagination'
 import Link from 'next/link'
 import { urlForImage } from '@/sanity/lib/image'
 import { useLanguage } from '@/store/use-language'
 import { Button } from '@/components/ui/button'
+import { Cols } from '@/components/cols'
+import { SectionTitle } from '@/components/section-title'
 
 export const About = ({ lectors }: { lectors: any }) => {
   const { language } = useLanguage()
@@ -31,19 +29,77 @@ export const About = ({ lectors }: { lectors: any }) => {
   return (
     <section id='about' className='scroll-mt-28'>
       <Container>
-        <InfoText
-          textCz='S naším lektorským týmem zajišťujeme ty nejlepší podmínky k tomu, abys měl*a možnost se co nejdříve rozmluvit. Máme pozitivní zkušenost s individuálními lekcemi nebo kurzy ve skupinách s opravdu nízkým počtem osob. Dostaneš se tak ke slovu dostatečně často, jazyk tě bude bavit a prostě se rozmluvíš. Více o tom, jak vzniklo naše studio se dozvíš'
-          textEn="Our team is providing the best conditions to get you talking fluently as soon as possible. We have a positive experience with both individual lessons or small group courses. At our studio, you'll get a word often enough and you will enjoy the language along. So, learn more about how our studio was established"
-          textDe='Mit unserem Dozententeam bieten wir dir die optimale Umgebung, damit du dich so schnell wie möglich in der fremden Sprache verständigen kannst. Wir haben positive Erfahrungen mit Einzelunterricht oder mit Gruppenkursen mit einer wirklich kleinen Anzahl an Teilnehmern. So kommst Du oft genug zu Wort, hast Spaß an der Sprache und redest dich einfach frei. Mehr darüber, wie unser Studio entstanden ist, erfährst du'
-          textUa='З нашою командою викладачів ми забезпечуємо найкращі умови, щоб ви мали можливість якнайшвидше розговоритися. У нас є позитивний досвід індивідуальних занять або курсів у групах з дійсно невеликою кількістю людей. Таким чином, ти зможеш говорити досить часто, тобі сподобається мова і ти просто будеш спілкуватися. Детальніше про те, як створювалася наша студія, можна дізнатися'
-          href='/story'
-          linkCz='zde ←'
-          linkEn='here ←'
-          linkDe='hier ←'
-          linkUa='тут ←'
-        />
+        <Cols>
+          <SectionTitle
+            titleCz='O nás'
+            titleEn='About us'
+            titleDe='Über uns'
+            titleUa='про нас'
+          />
+          <InfoText
+            textCz='S naším lektorským týmem zajišťujeme ty nejlepší podmínky k tomu, abys měl*a možnost se co nejdříve rozmluvit. Máme pozitivní zkušenost s individuálními lekcemi nebo kurzy ve skupinách s opravdu nízkým počtem osob. Dostaneš se tak ke slovu dostatečně často, jazyk tě bude bavit a prostě se rozmluvíš. Více o tom, jak vzniklo naše studio se dozvíš'
+            textEn="Our team is providing the best conditions to get you talking fluently as soon as possible. We have a positive experience with both individual lessons or small group courses. At our studio, you'll get a word often enough and you will enjoy the language along. So, learn more about how our studio was established"
+            textDe='Mit unserem Dozententeam bieten wir dir die optimale Umgebung, damit du dich so schnell wie möglich in der fremden Sprache verständigen kannst. Wir haben positive Erfahrungen mit Einzelunterricht oder mit Gruppenkursen mit einer wirklich kleinen Anzahl an Teilnehmern. So kommst Du oft genug zu Wort, hast Spaß an der Sprache und redest dich einfach frei. Mehr darüber, wie unser Studio entstanden ist, erfährst du'
+            textUa='З нашою командою викладачів ми забезпечуємо найкращі умови, щоб ви мали можливість якнайшвидше розговоритися. У нас є позитивний досвід індивідуальних занять або курсів у групах з дійсно невеликою кількістю людей. Таким чином, ти зможеш говорити досить часто, тобі сподобається мова і ти просто будеш спілкуватися. Детальніше про те, як створювалася наша студія, можна дізнатися'
+            href='/story'
+            linkCz='zde ←'
+            linkEn='here ←'
+            linkDe='hier ←'
+            linkUa='тут ←'
+          />
+        </Cols>
 
-        <Swiper
+        <div className='grid grid-cols-2 sm:grid-cols-4 gap-8 mt-14'>
+          {lectors.map((lector: any) => (
+            // <>
+            //   {lector.name ? (
+            <Link
+              key={lector.name}
+              href={`/lectors/${lector.slug.current}`}
+              className='grid place-content-center rounded-2xl'
+            >
+              <img
+                src={urlForImage(lector.image)}
+                alt={lector.name}
+                className='mb-4 w-full rounded-3xl'
+              />
+              <h3 className='font-stabil'>{lector.name}</h3>
+              <p className='font-stabil'>
+                {language === 'cz' && lector.roleCz}
+                {language === 'en' && lector.roleEn}
+                {language === 'de' && lector.roleDe}
+                {language === 'ua' && lector.roleUa}
+              </p>
+            </Link>
+            //   ) : (
+            //     <div
+            //       style={{ background: lector.color }}
+            //       className='grid place-content-center rounded-2xl text-center'
+            //     >
+            //       <h2 className='text-xl sm:text-3xl mb-8'>
+            //         {language === 'cz' && lector.titleCz}
+            //         {language === 'en' && lector.titleEn}
+            //         {language === 'de' && lector.titleDe}
+            //         {language === 'ua' && lector.titleUa}
+            //       </h2>
+            //       <Link href='/for-lectors'>
+            //         <Button
+            //           className='rounded-full bg-white hover:bg-zinc-100 text-lg sm:text-xl py-6 px-10'
+            //           size='lg'
+            //         >
+            //           {language === 'cz' && lector.buttonCz}
+            //           {language === 'en' && lector.buttonEn}
+            //           {language === 'de' && lector.buttonDe}
+            //           {language === 'ua' && lector.buttonUa}
+            //         </Button>
+            //       </Link>
+            //     </div>
+            //   )}
+            // </>
+          ))}
+        </div>
+
+        {/* <Swiper
           modules={[Pagination]}
           spaceBetween={70}
           slidesPerView={1}
@@ -53,7 +109,7 @@ export const About = ({ lectors }: { lectors: any }) => {
               slidesPerView: 2,
             },
             1536: {
-              slidesPerView: 3,
+              slidesPerView: 6,
             },
           }}
           className='mt-14 !pb-10 !select-none'
@@ -108,7 +164,7 @@ export const About = ({ lectors }: { lectors: any }) => {
           ))}
 
           <div className='swiper-pagination w-full space-x-2 !bottom-0' />
-        </Swiper>
+        </Swiper> */}
       </Container>
     </section>
   )
