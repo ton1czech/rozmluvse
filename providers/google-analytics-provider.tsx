ee3843612c355abcda48e45df5079054be2d2022
ton1czech <tonyasek007@gmail.com>
2024-04-19 15:09:57 +0200

    🐛BUG FIX: if window

--- providers/google-analytics-provider.tsx
+++ providers/google-analytics-provider.tsx
@@ -10,24 +10,25 @@ export const GoogleAnalyticsProvider = () => {
   return (
     <>
       {analyticsEnabled ||
-        (window?.localStorage
-          .getItem('cookies-storage')
-          ?.includes('"analyticsEnabled":true') && (
-          <>
-            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GTAG!} />
-            <Script
-              async
-              src='https://www.googletagmanager.com/gtag/js?id=G-K6EC52CP3Q'
-            />
-            <Script>
-              {`
+        (window &&
+          window?.localStorage
+            .getItem('cookies-storage')
+            ?.includes('"analyticsEnabled":true') && (
+            <>
+              <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GTAG!} />
+              <Script
+                async
+                src='https://www.googletagmanager.com/gtag/js?id=G-K6EC52CP3Q'
+              />
+              <Script>
+                {`
                 window.dataLayer = window.dataLayer || []; 
                 function gtag(){dataLayer.push(arguments);} 
                 gtag('js', new Date()); 
                 gtag('config', 'G-K6EC52CP3Q');`}
-            </Script>
-          </>
-        ))}
+              </Script>
+            </>
+          ))}
     </>
   )
 }
