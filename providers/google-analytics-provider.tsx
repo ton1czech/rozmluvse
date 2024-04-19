df9d0a364bea348d02d4d743ac23c3985abe6651
ton1czech <tonyasek007@gmail.com>
2024-04-19 15:12:01 +0200

    🐛BUG FIX: trying to fix that window issue

--- providers/google-analytics-provider.tsx
+++ providers/google-analytics-provider.tsx
@@ -3,32 +3,43 @@
 import { useCookies } from '@/store/use-cookies'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import Script from 'next/script'
+import { useEffect, useState } from 'react'
 
 export const GoogleAnalyticsProvider = () => {
   const { analyticsEnabled } = useCookies()
 
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  })
+
   return (
     <>
-      {analyticsEnabled ||
-        (window &&
-          window?.localStorage
-            .getItem('cookies-storage')
-            ?.includes('"analyticsEnabled":true') && (
-            <>
-              <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GTAG!} />
-              <Script
-                async
-                src='https://www.googletagmanager.com/gtag/js?id=G-K6EC52CP3Q'
-              />
-              <Script>
-                {`
+      {mounted && (
+        <>
+          {analyticsEnabled ||
+            (window &&
+              window?.localStorage
+                .getItem('cookies-storage')
+                ?.includes('"analyticsEnabled":true') && (
+                <>
+                  <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GTAG!} />
+                  <Script
+                    async
+                    src='https://www.googletagmanager.com/gtag/js?id=G-K6EC52CP3Q'
+                  />
+                  <Script>
+                    {`
                 window.dataLayer = window.dataLayer || []; 
                 function gtag(){dataLayer.push(arguments);} 
                 gtag('js', new Date()); 
                 gtag('config', 'G-K6EC52CP3Q');`}
-              </Script>
-            </>
-          ))}
+                  </Script>
+                </>
+              ))}
+        </>
+      )}
     </>
   )
 }
