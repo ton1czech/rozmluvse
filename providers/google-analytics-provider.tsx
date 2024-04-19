ab3c93d0e85461b1f36fac00d1dac55e1e1c373d
ton1czech <tonyasek007@gmail.com>
2024-04-19 10:03:59 +0200

    👌IMPROVE: ganalytics code to look for localstorage

--- providers/google-analytics-provider.tsx
+++ providers/google-analytics-provider.tsx
@@ -2,9 +2,32 @@
 
 import { useCookies } from '@/store/use-cookies'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
 
 export const GoogleAnalyticsProvider = () => {
   const { analyticsEnabled } = useCookies()
 
-  return <>{analyticsEnabled && <GoogleAnalytics gaId='G-K6EC52CP3Q' />}</>
+  return (
+    <>
+      {analyticsEnabled ||
+        (window.localStorage
+          .getItem('cookies-storage')
+          ?.includes('"analyticsEnabled":true') && (
+          <>
+            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GTAG!} />
+            <Script
+              async
+              src='https://www.googletagmanager.com/gtag/js?id=G-K6EC52CP3Q'
+            />
+            <Script>
+              {`
+                window.dataLayer = window.dataLayer || []; 
+                function gtag(){dataLayer.push(arguments);} 
+                gtag('js', new Date()); 
+                gtag('config', 'G-K6EC52CP3Q');`}
+            </Script>
+          </>
+        ))}
+    </>
+  )
 }
