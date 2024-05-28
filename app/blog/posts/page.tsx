11eb1070d497c1c91221c627da076c15191a50c6
ton1czech <tonyasek007@gmail.com>
2024-05-28 11:02:55 +0200

    🐛BUG FIX: blog posts suspense boundary

--- app/blog/posts/page.tsx
+++ app/blog/posts/page.tsx
@@ -1,13 +1,17 @@
 import { Posts } from '@/containers/posts'
 import { cachedClient } from '@/sanity/lib/client'
 import { PostsQuery } from '@/sanity/lib/queries'
+import { Loader2 } from 'lucide-react'
+import { Suspense } from 'react'
 
 export default async function Page() {
   const posts = await cachedClient(PostsQuery)
 
   return (
     <main className='mt-32 xl:mt-16 mb-8'>
-      <Posts posts={posts} />
+      <Suspense fallback={<Loader2 className='animate-spin' />}>
+        <Posts posts={posts} />
+      </Suspense>
     </main>
   )
 }
