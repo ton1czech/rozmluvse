82ada4c5398ad4d8a37372f951fd7cc6e1429723
ton1czech <tonyasek007@gmail.com>
2024-10-06 23:19:08 +0200

    ❌REMOVED: hero description text

--- containers/hero.tsx
+++ containers/hero.tsx
@@ -13,7 +13,7 @@ export const Hero = () => {
         <Cols>
           <div />
           <div>
-            <h1 className='text-3xl sm:text-4xl md:text-5xl font-black lg:text-6xl'>
+            <h1 className='text-3xl font-black sm:text-4xl md:text-5xl lg:text-6xl'>
               {language === 'cz' && (
                 <>Jazykové studio, ve kterém se nebudeš bát dělat chyby.</>
               )}
@@ -29,32 +29,6 @@ export const Hero = () => {
                 <>Мовні курси, де ти не будеш боятись помилитись.</>
               )}
             </h1>
-            <p className='font-stabil text-lg mt-8 xl:text-2xl'>
-              {language === 'cz' && (
-                <>
-                  Dopřej si lingvistický wellness a nauč se anglicky, německy
-                  nebo klidně čínsky.
-                </>
-              )}
-              {language === 'en' && (
-                <>
-                  Indulge in linguistic wellness and study English, German or
-                  even Chinese.
-                </>
-              )}
-              {language === 'de' && (
-                <>
-                  Gönne Dir linguistische Entspannung und lerne Englisch,
-                  Deutsch oder auch Chinesisch.
-                </>
-              )}
-              {language === 'ua' && (
-                <>
-                  Зроби собі "гарне лінгвістичне самопочуття" і вивчи
-                  англійську, німецьку або навіть китайську.
-                </>
-              )}
-            </p>
           </div>
         </Cols>
       </Container>
